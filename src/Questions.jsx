e2875796a87095a7bf1c9e3951260c53b788fd39
AiraJoy31 <eling011259@gmail.com>
2024-11-19 08:02:14 +0800

    quiz1

---
+++ src/Questions.jsx
@@ -0,0 +1,141 @@
+export const questions = [
+    {
+        question_aid: 1,
+        question_question: "Q1 Maganda ba ako?",
+        choices: [
+            {
+                choice: "Pwede",
+                isCorrect: false,
+            },  
+            {
+                choice: "Oo",
+                isCorrect: true,
+            },  
+            {
+                choice: "Sigruo",
+                isCorrect: false,
+            },  
+            {
+                choice: "Hindi",
+                isCorrect: false,
+            },  
+
+        ],
+    },
+    {
+        question_aid: 2,
+        question_question: "Q2 Papasa ba ako kay Maam Khate?",
+        choices: [
+            {
+                choice: "Pwede",
+                isCorrect: false,
+            },  
+            {
+                choice: "Oo",
+                isCorrect: true,
+            },  
+            {
+                choice: "Hinding Hindi",
+                isCorrect: false,
+            },  
+            {
+                choice: "Hindi",
+                isCorrect: false,
+            },  
+
+        ],
+    },
+    {
+        question_aid: 3,
+        question_question: "Q3 Ako ba si Joshua Garcia?",
+        choices: [
+            {
+                choice: "Pwede",
+                isCorrect: false,
+            },  
+            {
+                choice: "Oo",
+                isCorrect: true,
+            },  
+            {
+                choice: "Hinding Hindi",
+                isCorrect: false,
+            },  
+            {
+                choice: "Hindi",
+                isCorrect: false,
+            },  
+
+        ],
+    },
+    {
+        question_aid: 4,
+        question_question: "Q4 Ako ba yayaman ngayon?",
+        choices: [
+            {
+                choice: "Pwede",
+                isCorrect: true,
+            },  
+            {
+                choice: "Oo",
+                isCorrect: false,
+            },  
+            {
+                choice: "Hinding Hindi",
+                isCorrect: false,
+            },  
+            {
+                choice: "Hindi",
+                isCorrect: false,
+            },  
+
+        ],
+    },
+    {
+        question_aid: 5,
+        question_question: "Q5 May jeep ba mamaya sa kanto?",
+        choices: [
+            {
+                choice: "Meron",
+                isCorrect: true,
+            },  
+            {
+                choice: "Wala",
+                isCorrect: false,
+            },  
+            {
+                choice: "Maglakad kana",
+                isCorrect: false,
+            },  
+            {
+                choice: "hehe ewan",
+                isCorrect: false,
+            },  
+
+        ],
+    },
+    {
+        question_aid: 5,
+        question_question: "Q5 May jeep ba mamaya sa kanto?",
+        choices: [
+            {
+                choice: "Meron",
+                isCorrect: true,
+            },  
+            {
+                choice: "Wala",
+                isCorrect: false,
+            },  
+            {
+                choice: "Maglakad kana",
+                isCorrect: false,
+            },  
+            {
+                choice: "hehe ewan",
+                isCorrect: false,
+            },  
+
+        ],
+    },
+
+]
